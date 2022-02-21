1a97de128c5e1ab923d572a56e7e7fa303ebe552
Lincoln <Lincolnnovais15a@gmail.com>
2022-02-21 08:43:34 -0300

    parte 3

--- marcelo1/js/script.js
+++ marcelo1/js/script.js
@@ -1,4 +1,10 @@
 alert("Mensagem fora do arquivo");
 
-var idade;
-alert(idade);
+var idade; //Criação de variáveis
+var nome;
+
+nome = "Lincoln"
+idade = 17;
+alert("ola " +nome+ " voce tem " +idade+ " anos");
+
+document.write("ola " +nome+" você tem " +idade+" anos");
