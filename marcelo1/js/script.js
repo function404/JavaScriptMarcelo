ea00658291b16fc3000a5b06b443db4ee0d03279
LINCOLN NOVAIS MEZZALIRA <Lincoln.mezzalira@alunos.sc.senac.br>
2022-02-22 14:17:38 -0300

    atividade concluida

--- marcelo1/js/script.js
+++ marcelo1/js/script.js
@@ -1,10 +1,39 @@
-alert("Mensagem fora do arquivo 01");
+/* alert("Mensagem fora do arquivo 01");
 
-var idade; //Criação de variáveis
-var nome;
+var idade;     //variável tipo inteiro (int)
+var nome;      //varável tipo string
+var salario;   //variável tipo number/flot
+var booleano;  //variável tipo boolean
+var cidade;    //varável tipo string
 
-nome = "Lincoln"
+nome = "Lincoln";
 idade = 17;
-alert("ola " +nome+ " voce tem " +idade+ " anos");
+salario = 10.50; 
+booleano = true; //false
+cidade = "joinville";
 
-document.write("ola " +nome+" você tem " +idade+" anos");
+alert("ola " +nome+ " voce tem " +idade+ " anos");
+document.write("<h1>"+"ola " +nome+" você tem " +idade+" anos"+"</h1>");
+document.write("Olá "+nome+ " você tem "+idade+" anos e mora em "+cidade+" recebe "+salario+" por horas trabalhadas"+"<br>"); 
+document.write("Você gosta de morar em "+cidade+"? resposta: "+booleano); */
+
+var nome;
+var idade;
+var endereco;
+var peso;
+var altura;
+var mesada;
+
+nome = "Lincoln";
+idade = 17;
+endereco = "Rua Augusto Eccel 330";
+peso = 67.2;
+altura = 1.81;
+mesada = 100;
+
+document.write("<h1>"+"Óla me chamo "+nome+"."+"</h1>");
+document.write("<h1>"+"Eu tenho "+idade+" de idade."+"</h1>");
+document.write("<h1>"+"Eu moro na "+endereco+",");
+document.write("<h1>"+"Curiosidades de mim, eu peso "+peso+","+"</h1>");
+document.write("<h1>"+"minha altura é "+altura+","+"</h1>");
+document.write("<h1>"+"e eu ganho uma mesada de "+mesada+" por mês"+"</h1>");
