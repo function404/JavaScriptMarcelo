bb172e99cf478772f1e7e68bf304f244ae9acf39
Lincoln <Lincolnnovais15a@gmail.com>
2022-03-14 08:20:44 -0300

    ae ae ae ae pum pum

--- marcelo1/js/script.js
+++ marcelo1/js/script.js
@@ -17,6 +17,8 @@ document.write("<h1>"+"ola " +nome+" você tem " +idade+" anos"+"</h1>");
 document.write("Olá "+nome+ " você tem "+idade+" anos e mora em "+cidade+" recebe "+salario+" por horas trabalhadas"+"<br>"); 
 document.write("Você gosta de morar em "+cidade+"? resposta: "+booleano); */
 
+/*
+ 
 var nome;
 var idade;
 var endereco;
@@ -31,9 +33,50 @@ peso = 67.2;
 altura = 1.81;
 mesada = 100;
 
-document.write("<h1>"+"Óla me chamo "+nome+"."+"</h1>");
-document.write("<h1>"+"Eu tenho "+idade+" de idade."+"</h1>");
-document.write("<h1>"+"Eu moro na "+endereco+",");
-document.write("<h1>"+"Curiosidades de mim, eu peso "+peso+","+"</h1>");
-document.write("<h1>"+"minha altura é "+altura+","+"</h1>");
-document.write("<h1>"+"e eu ganho uma mesada de "+mesada+" por mês"+"</h1>");
+document.write("<h5>"+"Óla me chamo "+nome+"."+"</h5>");
+document.write("<h5>"+"Eu tenho "+idade+" de idade."+"</h5>");
+document.write("<h5>"+"Eu moro na "+endereco+","+"</h5>");
+document.write("<h5>"+"Curiosidades de mim, eu peso "+peso+","+"</h5>"); */
+
+/*
+var valorProduto = 30;
+
+if (valorProduto <= 10){
+    document.write('Permitido apenas compra à vista!');
+} else if (valorProduto > 10 && valorProduto <= 20){
+    document.write('Permitido financiamento em 3 vezes sem juros');
+} else if (valorProduto > 20 && valorProduto <= 60){
+    document.write('Permitido financiamento em 6 vezes sem juros');
+} else{
+    document.write('Permitido financiamento em 12 vezes sem juros');
+}
+==============================
+var idade = 19
+
+if (idade >= 18){
+    document.write("O Usuário não pode comprar, é menor de idade")
+} else if (idade >= 60) {
+    document.write("O Usuário não pode comprar, é melhor de idade")
+}
+else {
+    document.write("O usuário pode comprar")
+} */
+
+
+
+    var nmr = parseInt(prompt("Digite o código do filme:"));
+
+    if (nmr == 1){
+        document.write('Filme é The Batman, sala 01.');
+    } else if (nmr == 2){
+        document.write('Filme é Uncharted, sala 02.');
+    } else if (nmr == 3){
+        document.write('Filme é Ursinhos Carinhosos, sala 03.');
+    } else if (nmr == 4){
+        document.write('Filme é Exorcismo Sagrado, sala 04.');
+    } else if (nmr == 5){
+        document.write('Filme é Coração de Fogo, sala 05.');
+    } else{
+        document.write("Opção inválida!")
+    }
+
